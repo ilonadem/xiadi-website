51e6c7b33da1a9e36687a9ccab91e288f3bc1188
ilonadem <alenkademler@gmail.com>
2025-09-26 10:20:24 -0700

    beetle update

---
+++ tmp.py
@@ -0,0 +1,19 @@
+import cairosvg
+
+# 32×32 favicon
+cairosvg.svg2png(url="assets/beetle.svg",
+                 write_to="assets/beetle-32.png",
+                 output_width=32, output_height=32,
+                 background_color="transparent")
+
+# 16×16
+cairosvg.svg2png(url="assets/beetle.svg",
+                 write_to="assets/beetle-16.png",
+                 output_width=16, output_height=16,
+                 background_color="transparent")
+
+# 180×180 (Apple touch icon)
+cairosvg.svg2png(url="assets/beetle.svg",
+                 write_to="assets/beetle-apple-touch-icon.png",
+                 output_width=180, output_height=180,
+                 background_color="transparent")
